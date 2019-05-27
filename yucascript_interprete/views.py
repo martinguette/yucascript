4dca46323ff4ed60e195f9c473795f3ec982dfaf
Martin <zerisawa33@gmail.com>
2019-05-27 06:11:24 -0500

    django

--- yucascript_interprete/views.py
+++ yucascript_interprete/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
 def index(request):
-    return render(request, '../../../../frontend/index.html')
+    return render(request, 'yucascript_interprete/index.html')
+
+@csrf_exempt
+@require_http_methods([ "POST"])
+def validar(request):
+    if request.method == "POST":
+        data = request.POST
+        return JSONResponse(data, status=200)
