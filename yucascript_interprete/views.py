53678d1583dd0afab226d4e996ac47e3ee07fb09
Martin <zerisawa33@gmail.com>
2019-05-27 02:04:43 -0500

    django

--- yucascript_interprete/views.py
+++ yucascript_interprete/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'frontend/index.html')
+    return render(request, '../../../../frontend/index.html')
